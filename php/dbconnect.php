105b4b37e2404bbe6d0aeb98f4b676e61974ae7d
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-26 12:48:29 +0100

    Update dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,5 +1,14 @@
-<?php 
-  $db = mysqli_connect('localhost', 'root', '');
-  if ($db === FALSE ) die('False');
-  if (mysqli_select_db($db,"hotel") === FALSE ) die("Fail message");
-  ?>
+<?php
+$servername = "mysql.hostinger.kr";
+$database = "u375181454_hotel";
+$username = "u375181454_juhee";
+$password = "dst1738";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
