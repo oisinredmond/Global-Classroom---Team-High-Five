3aef7956660647f745a7458c459556042650116a
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-26 15:20:48 +0100

    Update search_rooms.php

--- php/search_rooms.php
+++ php/search_rooms.php
@@ -24,7 +24,17 @@
 	    <?php
 
 		
-				include 'dbconnect.php';
+				$servername = "mysql.hostinger.kr";
+$database = "u375181454_hotel";
+$username = "u375181454_juhee";
+$password = "dst1738";
+// Create connection
+$dbConnect = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$dbConnect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 
 	$query = "SELECT * FROM Rooms WHERE room_id NOT IN (
 			SELECT room_id FROM bookings
