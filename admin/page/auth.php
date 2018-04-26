cd9a76b1924b8a6d66bd164695c8fd38968254eb
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-27 00:13:02 +0100

    Update auth.php

--- admin/page/auth.php
+++ admin/page/auth.php
@@ -1,8 +1,8 @@
 
 <?php
 $servername = "mysql.hostinger.kr";
-$database = "u375181454_hotel";
-$username = "u375181454_juhee";
+$database = "u375181454_tempp";
+$username = "u375181454_tempp";
 $password = "dst1738";
 // Create connection
 $dbConnect = mysqli_connect($servername, $username, $password, $database);
@@ -10,7 +10,7 @@ $dbConnect = mysqli_connect($servername, $username, $password, $database);
 if (!$dbConnect) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$db = "u375181454_hotel";
+$db = "u375181454_tempp";
 $selected = mysqli_select_db($dbConnect,$db)
   or die("Could not select database");
 ?>
