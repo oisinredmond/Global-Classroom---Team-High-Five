4a7595842261f0c689866944d7097cdc0dbc346e
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-27 11:01:02 +0100

    Update loginauth.php

--- admin/page/loginauth.php
+++ admin/page/loginauth.php
@@ -4,7 +4,7 @@ $link  = mysqli_connect("mysql.hostinger.kr","u375181454_juhee", "dst1738", "u37
 $_SESSION['username'] = $_POST['username'];
 $_SESSION['password'] =  $_POST['password'];
 include './auth.php';
-$re = mysqli_query($link,"SELECT * from Staff where S_name = '".$_SESSION['username']."'  AND Password = '".$_SESSION['password']."' " );
+$re = mysqli_query($link,"SELECT * from staff where S_name = '".$_SESSION['username']."'  AND Password = '".$_SESSION['password']."' " );
 echo mysqli_error($link);
 if(mysqli_num_rows($re) > 0)
 {
