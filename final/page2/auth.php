b658361558bc5000dcf5e37b75ae9029e37da5ac
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-05-07 01:09:34 +0100

    Update auth.php

--- final/page2/auth.php
+++ final/page2/auth.php
@@ -1,17 +1,22 @@
 <?php
 
 
- $user = 'root';
- $passWord = '';
- $dbName = 'hotel';
 
- $dbConnect = new mysqli('localhost',$user,$passWord,$dbName);
- //connection to the database
 
-//select a database to work
-$db = "hotel";
-$selected = mysqli_select_db($dbConnect,$db)
-  or die(mysql_error());
 
+
+
+$servername = "mysql.hostinger.kr";
+$database = "u375181454_hotel";
+$username = "u375181454_juhee";
+$password = "dst1738";
+
+
+// Create connection
+$dbConnect = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$dbConnect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 ?>
