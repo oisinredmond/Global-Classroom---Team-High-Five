bec9666ed677becb1ae0e43815304fd1090d7a2e
fearlessdinosaur <c15436172@mydit.ie>
2018-05-07 16:12:44 +0200

    got email to work

--- sendMail.php
+++ sendMail.php
@@ -6,7 +6,12 @@ $sub = $_POST["subject"];
 
 $MSG = wordwrap($MSG,70);
 
-mail("placeholder@placeholder.com",$sub,$MSG,$header);
+mail("dcraigfox@hotmail.com",$sub,$MSG,$header);
 
+if(mail("dcraigfox@hotmail.com",$sub,$MSG,$header))
+{
+	echo "<h1> Thank you for your feedback </h1>";
+	echo "<script>setTimeout(\"location.href = 'http://localhost/Global-Classroom-Team-High-Five/main.php';\",1500);</script>";
+}
 
 ?>
