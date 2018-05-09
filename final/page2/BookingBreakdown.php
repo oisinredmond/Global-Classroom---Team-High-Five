0e7fafc6efa8021059792adacf66c4a1f58725c7
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-05-09 01:16:31 +0100

    Update BookingBreakdown.php

--- final/page2/BookingBreakdown.php
+++ final/page2/BookingBreakdown.php
@@ -3,7 +3,7 @@ session_start();
 
 
  include_once ("./basic.php");
-$link = mysqli_connect("localhost", "root", "", "hotel");
+
 
 include './auth.php';
 $re = mysqli_query($dbConnect,"SELECT * from users where User_id = '".$_SESSION['userID']."'  AND Password = '".$_SESSION['password']."' " );
