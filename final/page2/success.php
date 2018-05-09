23b668fef268ba427e24bd4ca2910b50852f2aab
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-05-09 02:32:43 +0100

    Update success.php

--- final/page2/success.php
+++ final/page2/success.php
@@ -4,7 +4,6 @@ session_start();
 
  include_once ("./basic.php");
 
-$link = mysqli_connect("localhost", "root", "", "hotel");
 include './auth.php';
 $re = mysqli_query($dbConnect,"SELECT * from users where User_id = '".$_SESSION['userID']."'  AND Password = '".$_SESSION['password']."' " );
 echo mysqli_error($dbConnect);
@@ -110,5 +109,5 @@ if (!$mail->send()) {
 } else {
     echo "Message sent!";
 }
-			header("location: successmessage.php");
+			header("location: successmessage.html");
 ?>
